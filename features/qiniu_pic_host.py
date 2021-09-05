e35372f8ce8ca01ab79bc879b71f3b01808e564a
早新 <ohiyox@gmail.com>
2021-09-05 15:44:42 +0800

    update

--- features/qiniu_pic_host.py
+++ features/qiniu_pic_host.py
@@ -27,14 +27,15 @@ import filetype
 from typing import Any
 
 current_path = os.path.dirname(__file__)
-with open(os.path.join(current_path,'qiniu_auth.json'),'r',encoding='UTF-8') as f:
+with open(os.path.join(current_path, 'qiniu_auth.json'), 'r', encoding='UTF-8') as f:
     auth_info = json.load(f)
     if not auth_info:
         exit('load auth_info failed.')
 
+
 class QnObS():
 
-    def __init__(self,auth_info):
+    def __init__(self, auth_info):
         self.access_key = auth_info['access_key']
         self.secret_key = auth_info['secret_key']
 
@@ -44,33 +45,31 @@ class QnObS():
         self.time_out = 3600
         self.q = None
 
-
     def connect(self):
-        self.q = Auth(self.access_key,self.secret_key)
+        self.q = Auth(self.access_key, self.secret_key)
         self.bucket_domain = self.get_bucket_domain(self.bucket_name)
 
     # 上传图片
-    def update(self,key,localfile,bucket_name='default_bucket') -> (bool,str):
+    def update(self, key, localfile, bucket_name='default_bucket') -> (bool, str):
         if bucket_name == 'default_bucket':
             bucket_name = self.bucket_name
-        root,filename = os.path.split(localfile)
-        print(f"正在上传：{filename} -> {key}",end='')
-        token = self.q.upload_token(bucket_name,key,self.time_out)
-        ret, info = put_file(token,key,localfile,version='v1')
+        root, filename = os.path.split(localfile)
+        print(f"正在上传：{filename} -> {key}", end='')
+        token = self.q.upload_token(bucket_name, key, self.time_out)
+        ret, info = put_file(token, key, localfile, version='v1')
         if (ret['key'] == key) and (ret['hash'] == etag(localfile)):
-            url = '/'.join([self.bucket_domain,key])
+            url = '/'.join([self.bucket_domain, key])
             print('\t上传完成。')
             return True, url
         else:
             url = ''
             return False, url
-        
-        
-    def get_file_info(self,key,bucket_name='default_bucket') -> dict:
+
+    def get_file_info(self, key, bucket_name='default_bucket') -> dict:
         if bucket_name == 'default_bucket':
             bucket_name = self.bucket_name
         bucket = BucketManager(self.q)
-        ret,info = bucket.stat(bucket_name,key)
+        ret, info = bucket.stat(bucket_name, key)
         return ret
 
     # 请求获取空间信息
@@ -87,7 +86,7 @@ class QnObS():
         ret = self.get_bucket_info(bucket_name)
         return json.loads(ret.text)[0]
 
-    def get_key_url(self,key,bucket_name='default_bucket'):
+    def get_key_url(self, key, bucket_name='default_bucket'):
         if bucket_name == "default_bucket":
             bucket_domain = self.bucket_domain
         else:
@@ -96,26 +95,25 @@ class QnObS():
         return f"http://{bucket_domain}/{key}"
 
     # localfile to url
-    def localfile_to_url(self,root, fpath) -> (bool,str):
+    def localfile_to_url(self, root, fpath) -> (bool, str):
         key = fpath.replace(root, '').lstrip('\\').lstrip('/')
-        timestamp = datetime.strftime(datetime.now(),'%Y%m%d')
-        key = '/'.join([timestamp,key])
-        return self.update(key,localfile=fpath)
-
+        timestamp = datetime.strftime(datetime.now(), '%Y%m%d')
+        key = '/'.join([timestamp, key])
+        return self.update(key, localfile=fpath)
 
 
 # 根据文件目录生成文件路径列表，并且在文件夹内生成excel。
 def localfile_to_excel(dirpath) -> str:
     result = os.walk(dirpath)
     items = []
-    for root,dirs,files in result:
+    for root, dirs, files in result:
         for file in files:
-            if filetype.guess_extension(os.path.join(root,file)) in ['png','jpg','gif']:
-                items.append((root,file))
+            if filetype.guess_extension(os.path.join(root, file)) in ['png', 'jpg', 'gif']:
+                items.append((root, file))
     df = pd.DataFrame([(dirpath, os.path.join(root, file)) for root, file in items], columns=['root', 'filepath'])
     df['url'] = ''
     excel_path = os.path.join(dirpath, 'images.xlsx')
-    df.to_excel(excel_path,engine='openpyxl')
+    df.to_excel(excel_path, engine='openpyxl')
 
     wb = load_workbook(excel_path)
     ws = wb.active
@@ -130,7 +128,7 @@ def localfile_to_excel(dirpath) -> str:
     return excel_path
 
 
-#直接对表格进行操作，生成url
+# 直接对表格进行操作，生成url
 def generate_url(excel_path):
     qos = QnObS(auth_info)
     qos.connect()
@@ -147,9 +145,9 @@ def generate_url(excel_path):
     filelist = ws[col_names['filepath']]
     lenfiles = len(filelist)
     blank_img = PIL.Image.new('RGB', (100, 100), color=(255, 255, 255))
-    for x in range(1,lenfiles):
+    for x in range(1, lenfiles):
         fpath = filelist[x].value
-        ws.row_dimensions[x+1].height = 70
+        ws.row_dimensions[x + 1].height = 70
         if os.path.exists(fpath):
             img = Image(fpath)
         else:
@@ -170,14 +168,15 @@ def generate_url(excel_path):
     root_list = ws[col_names['root']]
     lenfp = len(filepath_list)
     url_list = ws[col_names['url']]
-    for x in range(1,lenfp):
+    for x in range(1, lenfp):
         fpath = filepath_list[x].value
         root = root_list[x].value
-        result,url = qos.localfile_to_url(root=root,fpath=fpath)
+        result, url = qos.localfile_to_url(root=root, fpath=fpath)
         if result:
             url_list[x].value = url
 
     wb.save(excel_path)
+
 
 def run():
     def path_mode():
@@ -190,14 +189,15 @@ def run():
         generate_url(excelpath)
 
     mode_opts = {
-        'dir':path_mode,
-        'excel':excel_mode
+        'dir': path_mode,
+        'excel': excel_mode
     }
     mode = input('请选择模式（dir or excel）：')
     if mode in mode_opts.keys():
         mode_opts[mode]()
     else:
         exit('无法识别的模式。')
+
 
 if __name__ == '__main__':
     run()
